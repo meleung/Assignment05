cb6697d49142d7b100acf96bfc77b4ee3958cec5
Matthew Leung <matthew.e.leung@gmail.com>
2019-03-21 14:53:53 -0700

    Employee Management Functionally Complete

--- js/employee-management.js
+++ js/employee-management.js
@@ -5,88 +5,13 @@ var $ = function (id) {
     return window.document.getElementById(id);
 };
 
+/*** MODEL ***/
 var gEmployees = null;
-
-/* Update Employee Table
-  Must clean up THEAD / TBODY separation
-  */
-function showEmployees() {
-    "use strict";
-    var employeeTable, employeeTableBody, employeeTableTitle, i, row, employees;
-    
-    employees = JSON.parse(gEmployees);
-    
-    window.console.log(employees);
-    employeeTable = $("employee_table");
-    employeeTableBody = employeeTable.getElementsByTagName("TBODY")[0];
-    employeeTableTitle = $("table_title");
-    
-    window.console.log("table rows: " + employeeTable.rows.length);
-    window.console.log("table body: " + employeeTableBody.rows.length);
-    
-    /* Clear the table */
-    for (i = employeeTableBody.rows.length; i > 0; i -= 1) {
-        window.console.log(employeeTableBody.rows.length);
-        employeeTableBody.deleteRow(i - 1);
-    }
-    
-    /* Set the table title */
-    employeeTableTitle.innerHTML = "Showing ";
-    employeeTableTitle.innerHTML += employees.length;
-    employeeTableTitle.innerHTML += " Employee";
-    employeeTableTitle.innerHTML += employees.length === 1 ? "" : "s";
-    
-    /* Populate the table with current employees */
-    for (i = 0; i < employees.length; i += 1) {
-        row = employeeTableBody.insertRow(employeeTableBody.rows.length);
-        row.insertCell(0).innerHTML = employees[i].name;
-        row.insertCell(1).innerHTML = employees[i].title;
-        row.insertCell(2).innerHTML = employees[i].extension;
-        row.insertCell(3).innerHTML = "<input type='button' value='Delete'>";
-    }
-}
 
 function setEmployees(employees) {
     "use strict";
     //localStorage.setItem("employees", JSON.stringify(employees));
     gEmployees = JSON.stringify(employees);
-    showEmployees();
-}
-
-function deleteEmployee(employees, employee) {
-    "use strict";
-    var employeeIndex;
-    window.console.log("deleteEmployee()");
-    window.console.log($("employee_table"));
-    window.console.log(employee);
-    employeeIndex = $("employee_table").tBodies.indexOf(employee);
-    window.console.log(employeeIndex);
-    
-}
-
-function addEmployee(employees) {
-    "use strict";
-    //var nameField, titleField, extensionField,
-    var name, title, extension, employee;
-    
-    name = $("name").value;
-    title = $("title").value;
-    extension = parseInt($("ext").value, 10);
-    
-    if (name === "" || title === "" || isNaN(extension)) {
-        window.alert("Incomplete");
-    }
-    
-    employee = {
-        name : name,
-        title : title,
-        extension : extension
-    };
-    
-    employees.push(employee);
-    setEmployees(employees);
-    
-    window.console.log(employees);
 }
 
 function getEmployees() {
@@ -116,31 +41,128 @@ function getEmployees() {
             title : "Consultant",
             extension : 3516
         }];
-        setEmployees(employees);
+        //setEmployees(employees);
+    }
+
+    return employees;
+}
+
+function addEmployee(employees, employee) {
+    "use strict";
+
+    employees.push(employee);
+}
+
+function deleteEmployee(employees, employee) {
+    "use strict";
+    var e;
+
+    for (e in employees) {
+        if (employees.hasOwnProperty(e)) {
+            if (employees[e].name === employee.name && employees[e].title === employee.title && employees[e].extension === employee.extension) {
+                employees.splice(e, 1);
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+/*** VIEW ***/
+function showEmployees(employees) {
+    "use strict";
+    var employeeTable, employeeTableBody, employeeTableTitle, i, row;
+
+    employeeTable = $("employee_table");
+    employeeTableBody = employeeTable.getElementsByTagName("TBODY")[0];
+    employeeTableTitle = $("table_title");
+
+    /* Set the table title */
+    employeeTableTitle.innerHTML = "Showing ";
+    employeeTableTitle.innerHTML += employees.length;
+    employeeTableTitle.innerHTML += " Employee";
+    employeeTableTitle.innerHTML += employees.length === 1 ? "" : "s";
+    
+    employeeTableBody.parentElement.removeChild(employeeTableBody);
+    employeeTableBody = window.document.createElement("TBODY");
+    employeeTable.appendChild(employeeTableBody);
+
+    /* Populate the table with employees */
+    for (i = 0; i < employees.length; i += 1) {
+        row = employeeTableBody.insertRow(employeeTableBody.rows.length);
+        row.insertCell(0).innerHTML = employees[i].name;
+        row.insertCell(1).innerHTML = employees[i].title;
+        row.insertCell(2).innerHTML = employees[i].extension;
+        row.insertCell(3).innerHTML = "<input type='button' value='Delete'>";
+    }
+}
+
+/*** CONTROLLER ***/
+function addEmployeeHandler(e) {
+    "use strict";
+    var employees, employee, name, title, extension;
+    
+    e.preventDefault();
+    
+    name = $("name").value;
+    title = $("title").value;
+    extension = parseInt($("ext").value, 10);
+    
+    if (name === "" || title === "" || isNaN(extension)) {
+        window.alert("Incomplete");
+    }
+    employee = {
+        name : name,
+        title : title,
+        extension : extension
+    };
+    
+    employees = getEmployees();
+    addEmployee(employees, employee);
+    setEmployees(employees);
+    showEmployees(employees);
+}
+
+function deleteEmployeeHandler(e) {
+    "use strict";
+    var employees, employee, employeeRow, employeeFields, name, title, extension;
+    
+    /* Only checking for click event on the delete input button */
+    if (!(e.target.nodeName === "INPUT" && e.target.value === "Delete")) {
+        return;
     }
     
-    return employees;
+    employeeRow = e.target.parentElement.parentElement;
+    employeeFields = employeeRow.getElementsByTagName("TD");
+
+    name = employeeFields[0].innerHTML;
+    title = employeeFields[1].innerHTML;
+    extension = parseInt(employeeFields[2].innerHTML, 10);
+
+    employee = {
+        name: name,
+        title: title,
+        extension: extension
+    };
+
+    employees = getEmployees();
+    deleteEmployee(employees, employee);
+
+    employeeRow.parentElement.removeChild(employeeRow);
 }
 
 function init() {
     "use strict";
-    var addButton, employees, employeeTable;
+    var addButton, employeeTable, employees;
     
     addButton = $("add_button");
     employeeTable = $("employee_table");
     
     employees = getEmployees();
+    showEmployees(employees);
     
-    addButton.addEventListener("click", function (e) {
-        e.preventDefault();
-        addEmployee(employees);
-    });
-    
-    employeeTable.addEventListener("click", function (e) {
-        if (e.target.nodeName === "INPUT") {
-            deleteEmployee(employees, e.target.parentElement.parentElement);
-        }
-    });
+    addButton.addEventListener("click", addEmployeeHandler);
+    employeeTable.addEventListener("click", deleteEmployeeHandler);
 }
 
 window.addEventListener("load", init);
